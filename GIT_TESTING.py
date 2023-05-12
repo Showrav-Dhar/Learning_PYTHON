44b6016ba21e9c3e926e33439572b293627065e1
showrav dhar <dhardip349900@gmail.com>
2023-05-12 17:08:26 +0600

    update

--- GIT_TESTING.py
+++ GIT_TESTING.py
@@ -1,11 +1,12 @@
-print("Hey showrav dhar, are you enjoying learing python?")
-msg = "Showrav Dhar"
-print(msg.upper())
-print(msg.title())
-print(msg[1:5].upper())
-print("Enter Two Numbers\n")
-a = int(input())
-b = int(input())
-c = a+b
-str = f"sum of {a} and {b} = {c}"
-print(str)
+# print("Hey showrav dhar, are you enjoying learing python?")
+# msg = "Showrav Dhar"
+# print(msg.upper())
+# print(msg.title())
+# print(msg[1:5].upper())
+# print("Enter Two Numbers\n")
+# a = int(input())
+# b = int(input())
+# c = a+b
+# str = f"sum of {a} and {b} = {c}"
+# print(str)
+
---
+++ Tutorial.py
@@ -0,0 +1,12 @@
+#arithmatic operation
+a = 7
+b = 4;
+c = a+b
+print(f"Sum(+) of {a} {b} = ",c)
+print(f"Subtraction(-) of {a} {b} = ",a-b)
+print(f"Multiplication(*) of {a} {b} = ",a*b)
+print(f"Division(/) of {a} {b} = ",b/a)
+print(f"Modulo(%) of {a} {b} = ",a%b)
+
+print(f"Floor division(//) of {a} {b} = ",a//b)
+print(f"power operator( b ** a) -> {b} to the power {a} = ", b**a)
