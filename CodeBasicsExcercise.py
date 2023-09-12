2b9f42b42ee25544f432de4bd190f4f827f53f3f
showrav dhar <dhardip349900@gmail.com>
2023-09-12 12:38:52 +0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Tutorial.py

--- CodeBasicsExcercise.py
+++ CodeBasicsExcercise.py
@@ -67,37 +67,43 @@
 #         name = input()
 #         print(population[name])
 #         f = 0
+#
+# import statistics
+# stocks = {
+#     'info': [600, 630, 620],
+#     'ril': [1430, 1490, 1567],
+#     'mtl': [234, 180, 160]
+# }
+#
+# def print_all():
+#     for stock,price_list in stocks.items():
+#         avg = statistics.mean(price_list)
+#         print(f"{stock} ==> {price_list} ==> avg: ", round(avg, 2))
+#
+# def add():
+#     s = input("Enter a stock ticker to add:")
+#     p = input("Enter price of this stock:")
+#     p=float(p)
+#     if s in stocks:
+#         stocks[s].append(p)
+#     else:
+#         stocks[s] = [p]
+#     print_all()
+#
+# def main():
+#     op=input("Enter operation (print, add or amend):")
+#     if op.lower() == 'print':
+#         print_all()
+#     elif op.lower() == 'add':
+#         add()
+#     else:
+#         print("Unsupported operation:",op)
+#
+# if __name__ == '__main__':
+#     main()
 
-import statistics
-stocks = {
-    'info': [600, 630, 620],
-    'ril': [1430, 1490, 1567],
-    'mtl': [234, 180, 160]
-}
+#excecise on files
 
-def print_all():
-    for stock,price_list in stocks.items():
-        avg = statistics.mean(price_list)
-        print(f"{stock} ==> {price_list} ==> avg: ", round(avg, 2))
-
-def add():
-    s = input("Enter a stock ticker to add:")
-    p = input("Enter price of this stock:")
-    p=float(p)
-    if s in stocks:
-        stocks[s].append(p)
-    else:
-        stocks[s] = [p]
-    print_all()
-
-def main():
-    op=input("Enter operation (print, add or amend):")
-    if op.lower() == 'print':
-        print_all()
-    elif op.lower() == 'add':
-        add()
-    else:
-        print("Unsupported operation:",op)
-
-if __name__ == '__main__':
-    main()
+with open('myfile1','r') as f :
+    for line in f :
+        print(f)
