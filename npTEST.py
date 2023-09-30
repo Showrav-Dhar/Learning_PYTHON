a7d13badd78f55ccd77913a57df339ddfc83f45b
showrav dhar <dhardip349900@gmail.com>
2023-09-30 21:15:28 +0600

    Merge remote-tracking branch 'origin/main'

--- npTEST.py
+++ npTEST.py
@@ -1,8 +1,8 @@
 import numpy as np
 import sys
 import time
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     # sz = 10000
     #
     # test_list = range(sz)
@@ -27,20 +27,32 @@ if __name__ == '__main__':
     # res = a1+a2
     # print("numpy array took time - (in Ms)", (time.time() - start) * 1000)
 
-# video 2 - basic array operations
-    a = np.array([[1,2],[3,4],[5,6]],dtype=np.float64)
-    print(a)
-    print(a.shape)
-    print(a.dtype)
-    print(a.itemsize)
-    print(a.ndim)
-    print(a.size)
-    b = np.array([[1, 2], [3, 10], [100, 6]], dtype=complex)
-    print(b.dtype)
+    # video 2 - basic array operations
+    #     a = np.array([[1,2],[3,4],[5,6]],dtype=np.float64)
+    #     print(a)
+    #     print(a.shape)
+    #     print(a.dtype)
+    #     print(a.itemsize)
+    #     print(a.ndim)
+    #     print(a.size)
+    #     b = np.array([[1, 2], [3, 10], [100, 6]], dtype=complex)
+    #     print(b.dtype)
 
+    # print("HELLO")
+    # a = np.array([[1,2],[3,4],[4,5]])
+    # print("Shape of a = ",a.shape)
+    # print(a)
+    #
+    # b = a.reshape(2,3)
+    # print("Changed shape of a = ",b.shape)
+    # print(b)
 
-
-
-
-
-
+    # a = np.zeros((4,4))
+    # print(a)
+    a = np.array([[9, 4], [25, 36], [49, 64]])
+    print(np.sqrt(a))
+    print(np.std(a))
+    print(a.sum(axis=0))  # axis = 0 (column wise)
+    print(a.sum(axis=1))  # axis = 0 (row wise)
+    b = a.sum(axis=0)
+    print(b)
