435af3298d872ff19d6105903db5cfb5beca5d54
showrav dhar <dhardip349900@gmail.com>
2024-02-27 18:27:59 +0600

    Romania Map BFS - AI LAB Assignment

--- Python_Tutorial_Hitesh_Chaudhury/RomaniaMap_A_Star_Algorithm.py
+++ Python_Tutorial_Hitesh_Chaudhury/RomaniaMap_A_Star_Algorithm.py
@@ -1,62 +1,140 @@
 import heapq
-# Define the map of Romania with distances between cities
-romania_map = {
-    'Arad': {'Zerind': 75, 'Timisoara': 118, 'Sibiu': 140},
-    'Zerind': {'Arad': 75, 'Oradea': 71},
-    'Timisoara': {'Arad': 118, 'Lugoj': 111},
-    'Sibiu': {'Arad': 140, 'Oradea': 151, 'Fagaras': 99, 'Rimnicu Vilcea': 80},
-    'Oradea': {'Zerind': 71, 'Sibiu': 151},
-    'Lugoj': {'Timisoara': 111, 'Mehadia': 70},
-    'Fagaras': {'Sibiu': 99, 'Bucharest': 211},
-    'Rimnicu Vilcea': {'Sibiu': 80, 'Pitesti': 97, 'Craiova': 146},
-    'Mehadia': {'Lugoj': 70, 'Drobeta': 75},
-    'Drobeta': {'Mehadia': 75, 'Craiova': 120},
-    'Craiova': {'Drobeta': 120, 'Rimnicu Vilcea': 146, 'Pitesti': 138},
-    'Pitesti': {'Rimnicu Vilcea': 97, 'Craiova': 138, 'Bucharest': 101},
-    'Bucharest': {'Fagaras': 211, 'Pitesti': 101, 'Giurgiu': 90, 'Urziceni': 85},
-    'Giurgiu': {'Bucharest': 90},
-    'Urziceni': {'Bucharest': 85, 'Hirsova': 98, 'Vaslui': 142},
-    'Hirsova': {'Urziceni': 98, 'Eforie': 86},
-    'Eforie': {'Hirsova': 86},
-    'Vaslui': {'Urziceni': 142, 'Iasi': 92},
-    'Iasi': {'Vaslui': 92, 'Neamt': 87},
-    'Neamt': {'Iasi': 87}
-}
-class Node:
-    def __init__(self, city, cost, parent=None):
-        self.city = city
-        self.cost = cost
-        self.parent = parent
-    def __lt__(self, other):
-        return self.cost < other.cost
-    def heuristic(node, goal):
-        return 0 # No need for heuristic in this case
-    def astar_search(graph, start, goal):
-        open_list = []
-        closed_set = set()
-        heapq.heappush(open_list, start)
-        while open_list:
-            current_node = heapq.heappop(open_list)
-            if current_node.city == goal.city:
-                path = []
-                while current_node:
-                    path.append(current_node.city)
-                    current_node = current_node.parent
-                return path[::-1] # Reverse the path to get it from start to goal
-            closed_set.add(current_node.city)
-            for neighbor, distance in graph[current_node.city].items():
-                if neighbor not in closed_set:
-                    new_cost = current_node.cost + distance
-                    new_node = Node(neighbor, new_cost, current_node)
-                    heapq.heappush(open_list, new_node)
-        return None # No path found
 
-start_city = 'Arad'
-goal_city = 'Bucharest'
-start_node = Node(start_city, 0)
-goal_node = Node(goal_city, 0)
-path = astar_search(romania_map, start_node, goal_node)
-if path:
-print("Path found:", path)
-else:
-print("No path found")
+
+class priorityQueue:
+    def __init__(self):
+        self.cities = []
+
+    def push(self, city, cost):
+        heapq.heappush(self.cities, (cost, city))
+
+    def pop(self):
+        return heapq.heappop(self.cities)[1]
+
+    def isEmpty(self):
+        if (self.cities == []):
+            return True
+        else:
+            return False
+
+    def check(self):
+        print(self.cities)
+
+
+romania = {
+    "Arad": [("Zerind", 75), ("Sibiu", 140), ("Timisoara", 118), ],
+    "Zerind": [("Arad", 75), ("Oradea", 71), ],
+    "Oradea": [("Zerind", 71), ("Sibiu", 151), ],
+    "Sibiu": [("Arad", 140), ("Oradea", 151), ("Fagaras", 99), ("Rimnicu Vilcea", 80), ],
+    "Timisoara": [("Arad", 118), ("Lugoj", 111), ],
+    "Lugoj": [("Timisoara", 111), ("Mehadia", 70), ],
+    "Fagaras": [("Sibiu", 99), ("Bucharest", 211), ],
+    "Rimnicu Vilcea": [("Sibiu", 80), ("Pitesti", 97), ("Craiova", 146), ],
+    "Mehadia": [("Lugoj", 70), ("Dobreta", 75), ],
+    "Bucharest": [("Fagaras", 211), ("Pitesti", 101), ("Urziceni", 85), ("Giurglu", 90), ],
+    "Pitesti": [("Bucharest", 101), ("Rimnicu Vilcea", 97), ("Craiova", 138), ],
+    "Craiova": [("Rimnicu Vilcea", 146), ("Pitesti", 138), ("Dobreta", 120), ],
+    "Urziceni": [("Bucharest", 85), ("Hirsova", 98), ("Vaslui", 142), ],
+    "Hirsova": [("Urziceni", 98), ("Eforie", 86), ],
+    "Vaslui": [("Urziceni", 142), ("Lasi", 92), ],
+    "Lasi": [("Vaslui", 92), ("Neamt", 87), ],
+    "Dobreta": [("Mehadia", 75), ("Craiova", 120), ],
+    "Eforie": [("Hirsova", 86), ],
+    "Neamt": [("Lasi", 87), ],
+}
+
+huristic = {
+    "Arad": 366,
+    "Bucharest": 0,
+    "Craiova": 160,
+    "Dobreta": 242,
+    "Eforie": 161,
+    "Fagaras": 176,
+    "Giurgiu": 77,
+    "Hirsova": 151,
+    "Lasi": 226,
+    "Lugoj": 244,
+    "Mehadia": 241,
+    "Neamt": 234,
+    "Oradea": 380,
+    "Pitesti": 100,
+    "Rimnicu Vilcea": 193,
+    "Sibiu": 253,
+    "Timisoara": 329,
+    "Urziceni": 80,
+    "Vaslui": 199,
+    "Zerind": 374,
+}
+
+
+def heuristic(node, values):
+    return values[node]
+
+
+def astar(start, end):
+    path = {}
+    distance = {}
+    q = priorityQueue()
+    h = huristic
+
+    q.push(start, 0)
+    distance[start] = 0
+    path[start] = None
+    expandedList = []
+
+    while (q.isEmpty() == False):
+        current = q.pop()
+        expandedList.append(current)
+
+        if (current == end):
+            break
+
+        for new_city, new_distance in romania[current]:  # Access city and distance from tuple
+            g_cost = distance[current] + new_distance
+
+            if new_city not in distance or g_cost < distance[new_city]:
+                distance[new_city] = g_cost
+                f_cost = g_cost + heuristic(new_city, h)
+                q.push(new_city, f_cost)
+                path[new_city] = current
+
+    # printoutput(start, end, path, distance, expandedList)
+    finalpath = []
+    i = end
+
+    while (path.get(i) != None):
+        finalpath.append(i)
+        i = path[i]
+    finalpath.append(start)
+    finalpath.reverse()
+    print("Cities passed with the shortest distance\t: " + str(finalpath))
+    print("Number of cities passed through \t\t\t: " + str(len(finalpath)))
+    print("Total distance \t\t\t\t\t\t: " + str(distance[end]))
+
+# def printoutput(start, end, path, distance, expandedlist):
+#     finalpath = []
+#     i = end
+#
+#     while (path.get(i) != None):
+#         finalpath.append(i)
+#         i = path[i]
+#     finalpath.append(start)
+#     finalpath.reverse()
+#
+#     print("From Arad To Bucharest")
+#     print("=======================================================")
+#     print("Cities that might be explored \t\t: " + str(expandedlist))
+#     print("Number of cities passed through \t\t: " + str(len(expandedlist)))
+#     print("=======================================================")
+#     print("Cities passed with the shortest distance\t: " + str(finalpath))
+#     print("Number of cities passed through \t\t\t: " + str(len(finalpath)))
+#     print("Total distance \t\t\t\t\t\t: " + str(distance[end]))
+
+
+if __name__ == "__main__":
+    src = "Arad"
+    dst = "Bucharest"
+    print("After applying A* algorithm from Arad To Bucharest - ")
+    astar(src, dst)
+
+    # modify for university
--- Python_Tutorial_Hitesh_Chaudhury/t1.py
+++ Python_Tutorial_Hitesh_Chaudhury/t1.py
@@ -133,72 +133,182 @@
 #     for j in range(len(res[i])):
 #         print(res[i][j],end='')
 #         print()
+#
+# from queue import Queue
+#
+#
+# def GreedyBFS(romaniaMap, StartingNode, DestinationNode):
+#     visited = {}    # for keeping track of the visited nodes,
+#     distance = {}   # keeping track of the level from root node
+#     parent = {}     # keeping track of the parents of a node
+#
+#     BFS_result = []     # result of level order traversal
+#     queue = Queue()     # to maintain level order traversal
+#
+#     for city in romaniaMap.keys():
+#         visited[city] = False   # initially making every city unvisited
+#         distance[city] = -1     # initially making every city level = -1
+#         parent[city] = None     # initially making every city with no parent
+#
+#     StartingCity = StartingNode
+#     visited[StartingCity] = True    # Marked the starting city visited
+#     distance[StartingCity] = 0      # root node level = 0
+#     queue.put(StartingCity)         # start of bfs queue
+#
+#     while not queue.empty():
+#         CurrentCity = queue.get()     # taking out the first item of the queue
+#         BFS_result.append(CurrentCity)
+#
+#         for Neighbour in romaniaMap[CurrentCity]:     # visiting adjacent nodes of CurrentCity
+#             if not visited[Neighbour]:
+#                 visited[Neighbour] = True
+#                 parent[Neighbour] = CurrentCity
+#                 distance[Neighbour] = distance[CurrentCity] + 1
+#                 queue.put(Neighbour)
+#
+#     g = DestinationNode
+#     finalResult = []
+#     while g is not None:    # g will be None for the starting city
+#         finalResult.append(g)   # going backwards
+#         g = parent[g]
+#
+#     finalResult.reverse()
+#     print(finalResult)
+#
+#
+# if __name__ == '__main__':
+#     romaniaMap = {
+#         'Arad': ['Sibiu', 'Zerind', 'Timisoara'],
+#         'Zerind': ['Arad', 'Oradea'],
+#         'Oradea': ['Zerind', 'Sibiu'],
+#         'Sibiu': ['Arad', 'Oradea', 'Fagaras', 'Rimnicu'],
+#         'Timisoara': ['Arad', 'Lugoj'],
+#         'Lugoj': ['Timisoara', 'Mehadia'],
+#         'Mehadia': ['Lugoj', 'Drobeta'],
+#         'Drobeta': ['Mehadia', 'Craiova'],
+#         'Craiova': ['Drobeta', 'Rimnicu', 'Pitesti'],
+#         'Rimnicu': ['Sibiu', 'Craiova', 'Pitesti'],
+#         'Fagaras': ['Sibiu', 'Bucharest'],
+#         'Pitesti': ['Rimnicu', 'Craiova', 'Bucharest'],
+#         'Bucharest': ['Fagaras', 'Pitesti', 'Giurgiu', 'Urziceni'],
+#         'Giurgiu': ['Bucharest'],
+#         'Urziceni': ['Bucharest', 'Vaslui', 'Hirsova'],
+#         'Hirsova': ['Urziceni', 'Eforie'],
+#         'Eforie': ['Hirsova'],
+#         'Vaslui': ['Iasi', 'Urziceni'],
+#         'Iasi': ['Vaslui', 'Neamt'],
+#         'Neamt': ['Iasi']
+#     }
+#     StartNode = 'Arad'
+#     DestinationNode = 'Bucharest'
+#     GreedyBFS(romaniaMap, StartNode, DestinationNode)
 
-from queue import Queue
+import heapq
 
 
-def GreedyBFS(romaniaMap, StartingNode, DestinationNode):
-    visited = {}    # for keeping track of the visited nodes,
-    distance = {}   # keeping track of the level from root node
-    parent = {}     # keeping track of the parents of a node
+class priorityQueue:
+    def __init__(self):
+        self.cities = []
 
-    BFS_result = []     # result of level order traversal
-    queue = Queue()     # to maintain level order traversal
+    def push(self, city, cost):  # to add a tuple (cost, city) to the self.cities , MinHeap Tree
+        heapq.heappush(self.cities, (cost, city))
 
-    for city in romaniaMap.keys():
-        visited[city] = False   # initially making every city unvisited
-        distance[city] = -1     # initially making every city level = -1
-        parent[city] = None     # initially making every city with no parent
+    def pop(self):
+        return heapq.heappop(self.cities)[1]
 
-    StartingCity = StartingNode
-    visited[StartingCity] = True    # Marked the starting city visited
-    distance[StartingCity] = 0      # root node level = 0
-    queue.put(StartingCity)         # start of bfs queue
+    def isEmpty(self):
+        if self.cities == []:
+            return True
+        else:
+            return False
 
-    while not queue.empty():
-        CurrentCity = queue.get()     # taking out the first item of the queue
-        BFS_result.append(CurrentCity)
-
-        for Neighbour in romaniaMap[CurrentCity]:     # visiting adjacent nodes of CurrentCity
-            if not visited[Neighbour]:
-                visited[Neighbour] = True
-                parent[Neighbour] = CurrentCity
-                distance[Neighbour] = distance[CurrentCity] + 1
-                queue.put(Neighbour)
-
-    g = DestinationNode
-    finalResult = []
-    while g is not None:    # g will be None for the starting city
-        finalResult.append(g)   # going backwards
-        g = parent[g]
-
-    finalResult.reverse()
-    print(finalResult)
+    def check(self):
+        print(self.cities)
 
 
-if __name__ == '__main__':
-    romaniaMap = {
-        'Arad': ['Sibiu', 'Zerind', 'Timisoara'],
-        'Zerind': ['Arad', 'Oradea'],
-        'Oradea': ['Zerind', 'Sibiu'],
-        'Sibiu': ['Arad', 'Oradea', 'Fagaras', 'Rimnicu'],
-        'Timisoara': ['Arad', 'Lugoj'],
-        'Lugoj': ['Timisoara', 'Mehadia'],
-        'Mehadia': ['Lugoj', 'Drobeta'],
-        'Drobeta': ['Mehadia', 'Craiova'],
-        'Craiova': ['Drobeta', 'Rimnicu', 'Pitesti'],
-        'Rimnicu': ['Sibiu', 'Craiova', 'Pitesti'],
-        'Fagaras': ['Sibiu', 'Bucharest'],
-        'Pitesti': ['Rimnicu', 'Craiova', 'Bucharest'],
-        'Bucharest': ['Fagaras', 'Pitesti', 'Giurgiu', 'Urziceni'],
-        'Giurgiu': ['Bucharest'],
-        'Urziceni': ['Bucharest', 'Vaslui', 'Hirsova'],
-        'Hirsova': ['Urziceni', 'Eforie'],
-        'Eforie': ['Hirsova'],
-        'Vaslui': ['Iasi', 'Urziceni'],
-        'Iasi': ['Vaslui', 'Neamt'],
-        'Neamt': ['Iasi']
+def heuristic(node, values):
+    return values[node]
+
+
+# takes the huristics dictionary, returns the straight line distance from a node to bucharest
+
+def astar(start, end, romania, huristics):
+    path = {}  # keeps track of parent of each city
+    distance = {}  # keeps track of distance from Arad to each city
+    q = priorityQueue()
+    h = huristics  # copy of huristics dictionary
+
+    q.push(start, 0)
+    distance[start] = 0
+    path[start] = None
+    expandedList = []
+
+    while not q.isEmpty():  # loop will run until q is empty
+        current = q.pop()
+        expandedList.append(current)
+
+        if current == end:  # Loop Break if we reach the end city
+            break
+
+        #   exploring neighbours , new_city = neighbor of current city
+        for new_city, new_distance in romania[current]:  # Access city and distance from tuple
+            g_cost = distance[current] + new_distance  # g(n) = distance from start node to current node
+
+            if new_city not in distance or g_cost < distance[new_city]:
+                distance[new_city] = g_cost
+                f_cost = g_cost + heuristic(new_city, h)  # f(n) = g(n) + h(n)
+                q.push(new_city, f_cost)  # Add neighbor to queue
+                path[new_city] = current  # Update the parent of the neighbor
+
+    finalpath = []
+    i = end
+
+    while path.get(i) is not None:  # Backtracking to start node
+        finalpath.append(i)
+        i = path[i]
+    finalpath.append(start)
+    finalpath.reverse()
+
+    print("Cities that might be explored : " + str(expandedList))
+    print("Number of cities passed through : " + str(len(expandedList)))
+    print("Cities passed with the shortest distance : " + str(finalpath))
+    print("Number of cities passed through : " + str(len(finalpath)))
+    print("Total distance : " + str(distance[end]))
+
+
+if __name__ == "__main__":
+
+    RomaniaMap = {
+        "Arad": [("Zerind", 75), ("Sibiu", 140), ("Timisoara", 118), ],
+        "Zerind": [("Arad", 75), ("Oradea", 71), ],
+        "Oradea": [("Zerind", 71), ("Sibiu", 151), ],
+        "Sibiu": [("Arad", 140), ("Oradea", 151), ("Fagaras", 99), ("Rimnicu Vilcea", 80), ],
+        "Timisoara": [("Arad", 118), ("Lugoj", 111), ],
+        "Lugoj": [("Timisoara", 111), ("Mehadia", 70), ],
+        "Fagaras": [("Sibiu", 99), ("Bucharest", 211), ],
+        "Rimnicu Vilcea": [("Sibiu", 80), ("Pitesti", 97), ("Craiova", 146), ],
+        "Mehadia": [("Lugoj", 70), ("Dobreta", 75), ],
+        "Bucharest": [("Fagaras", 211), ("Pitesti", 101), ("Urziceni", 85), ("Giurglu", 90), ],
+        "Pitesti": [("Bucharest", 101), ("Rimnicu Vilcea", 97), ("Craiova", 138), ],
+        "Craiova": [("Rimnicu Vilcea", 146), ("Pitesti", 138), ("Dobreta", 120), ],
+        "Urziceni": [("Bucharest", 85), ("Hirsova", 98), ("Vaslui", 142), ],
+        "Hirsova": [("Urziceni", 98), ("Eforie", 86), ],
+        "Vaslui": [("Urziceni", 142), ("Lasi", 92), ],
+        "Lasi": [("Vaslui", 92), ("Neamt", 87), ],
+        "Dobreta": [("Mehadia", 75), ("Craiova", 120), ],
+        "Eforie": [("Hirsova", 86), ],
+        "Neamt": [("Lasi", 87), ],
     }
-    StartNode = 'Arad'
-    DestinationNode = 'Bucharest'
-    GreedyBFS(romaniaMap, StartNode, DestinationNode)
+
+    HuristicValues = {
+        "Arad": 366, "Bucharest": 0, "Craiova": 160, "Dobreta": 242, "Eforie": 161,
+        "Fagaras": 176, "Giurgiu": 77, "Hirsova": 151, "Lasi": 226, "Lugoj": 244,
+        "Mehadia": 241, "Neamt": 234, "Oradea": 380, "Pitesti": 100,
+        "Rimnicu Vilcea": 193, "Sibiu": 253, "Timisoara": 329,
+        "Urziceni": 80, "Vaslui": 199, "Zerind": 374,
+    }
+
+    src = "Arad"
+    dst = "Bucharest"
+    print("\nAfter applying A* algorithm from Arad To Bucharest - \n")
+    astar(src, dst, RomaniaMap, HuristicValues)
